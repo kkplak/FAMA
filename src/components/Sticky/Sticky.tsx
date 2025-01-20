a5f3c98b7d7051769f8a1c842e2a21981824124c
Konrad P <konrad.plak@tmsw.com>
2025-01-20 22:42:32 +0100

    nav fix

--- src/components/Sticky/Sticky.tsx
+++ src/components/Sticky/Sticky.tsx
@@ -55,7 +55,7 @@ const Sticky: React.FC<StickyProps> = ({ children, offset }) => {
         top: isSticky ? `${offset}px` : "50%",
         left: "50%",
         transform: isSticky ? "translateX(-50%)" : "translate(-50%, -50%)",
-        zIndex: 20,
+        zIndex: 50,
       }}
     >
       {children}
